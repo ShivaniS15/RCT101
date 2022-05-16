eccea7bc6ec04247249189530a93c25cee2b4793
ShivaniS15 <shivaniekta15@gmail.com>
2022-05-17 00:13:46 +0530

    done

--- 1_1_mobileos/src/App.js
+++ 1_1_mobileos/src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
-import Functional from './Components/Functional.jsx';
+import Functional,{Functional1} from './Components/Functional.jsx';
 
 function App() {
+  let info = [
+    { os: "Android" },
+    { os: "Blackberry" },
+    { os: "iPhone" },
+    { os: "Windows Phone" }
+  ];
+
+  let info1 = [
+    { mm: "Samsung" },
+    { mm: "HTC" },
+    { mm: "Micromax" },
+    { mm: "Apple" }
+  ];
+
   return (
     <div className="App">
-      <Functional />
+      <h1>Mobile Operating System</h1>
+        <Functional {...info[0]} />
+        <Functional {...info[1]} />
+        <Functional {...info[2]} />
+        <Functional {...info[3]} />
+      <h1>Mobile Manufactures</h1>
+      <Functional1 {...info1[0]} />
+      <Functional1 {...info1[1]} />
+      <Functional1 {...info1[2]} />
+      <Functional1 {...info1[3]} />
     </div>
   );
 }
--- 1_1_mobileos/src/Components/Functional.jsx
+++ 1_1_mobileos/src/Components/Functional.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React from 'react';
 
-const Functional =()=>{
-    return <h1>Hello</h1>
+const Functional = (props) => {
+    return (
+        <div>
+            <ul type="bullet">
+                <li> {props.os}</li>
+            </ul>
+        </div>
+        
+    )
+};
+
+const Functional1 = (props) => {
+    return (
+        <div>
+            <ul type="bullet">
+                <li> {props.mm}</li>
+            </ul>
+        </div>
+        
+    )
 }
 
 export default Functional;
+export {Functional1}
